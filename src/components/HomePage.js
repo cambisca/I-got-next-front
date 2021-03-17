010a824ff33ad2f3c61c0e24bb93781603de869f
Cameron Bisca <cjbisca@gmail.com>
2021-03-17 17:07:57 -0400

    background image on homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,25 +1,11 @@
 import React from 'react'
+import NavBar from './NavBar'
 
 function Homepage(){
     return (
         <div class="home-wrapper">
-            <div class="nav">
-                <div class="option opt-logo">
-                    I GOT NEXT
-                </div>
-                <div class="option opt-discover">
-                    Discover
-                </div>
-                <div class="option opt-login">
-                    Login
-                </div>
-            </div>
-            <div class="background-video">
-                <video autoplay muted allowfullscreen id="home-video">
-                    {/* <source src="https://www.youtube.com/embed/GGPH_qf4awk"></source> */}
-                    <iframe src="https://www.youtube.com/embed/GGPH_qf4awk" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                </video>
-            </div>
+            <NavBar />
+            
         </div>
     )
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function NavBar(){
+    return (
+        <div class="nav">
+                <div class="option opt-logo">
+                    I GOT NEXT
+                </div>
+                
+                <div>
+
+                </div>
+                <div class="option opt-discover">
+                    Discover
+                </div>
+                <div class="option opt-login">
+                    Login
+                </div>
+        </div>
+
+            
+    )
+    
+}
+
+
+export default NavBar;
