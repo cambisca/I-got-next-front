5cb6ee7de50523d3ba3c805052a53f49b0c35d6f
Cameron Bisca <cjbisca@gmail.com>
2021-07-06 18:22:48 -0400

    remodel homepage with image background

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,16 +6,11 @@ function Homepage({currentUser}){
 
     return (
         <div class="home-wrapper">
-            <div class="homepage-filler" align="center"></div>
-
-            <div class="home-page-gif">
-                <img class="home-page-gif" src="https://i.pinimg.com/originals/fd/04/54/fd0454c0d852df7c1b70ab8caa394632.gif"></img>
-            </div>
-
-            <div class="homepage-filler" align="center">
+            <div class="homepage-login" >
                 {currentUser ? null : <button class="login-button homepage-login-button">
                     <Link to="/login" className="login-button-text"> Login </Link>
                 </button>}
+
             </div>
         </div>
     )
